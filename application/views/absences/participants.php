7c8c17d70199a1e83a1bbe606ecdf754f9b78ed2
Fadel Al Fayed <fadelalfayed27@gmail.com>
2022-10-18 09:09:51 +0700

    Upload resi konsumsi

--- application/views/absences/participants.php
+++ application/views/absences/participants.php
@@ -7,6 +7,13 @@
 		max-width: 1400px !important;
 	}
 
+	.small-file-upload {
+		font-size: 11px !important;
+		display: inline-block;
+		width: 5.7rem;
+		padding: 5px;
+	}
+
 </style>
 
 <div class="post d-flex flex-column-fluid" id="kt_post">
@@ -70,14 +77,16 @@
 									<tfoot>
 										<tr style="padding-right: 2rem">
 											<td colspan="5" style="text-align: right;">Total :</td>
-											<td class="total text-danger" style="text-align: right;">150.000</td>
-											<td class="total_internet text-danger">
+											<td class="total text-danger text-center">
+												150.000
+											</td>
+											<td class="total_internet text-danger text-center">
 												250.000
 											</td>
-											<td class="total_other text-danger">
+											<td class="total_other text-danger text-center">
 												50.000
 											</td>
-											<td class="grand_total text-danger">
+											<td class="grand_total text-danger text-center">
 												450.000
 											</td>
 										</tr>
@@ -146,26 +155,39 @@
 					orderable: false,
 					searchable: false,
 					render: function (data, _, row) {
+						const receipt = data
+						let receiptDetail = ''
+						let uploadBtn = 'd-none'
+						if (!receipt || receipt == '' || receipt == null) {
+							uploadBtn = ''
+							receiptDetail = 'd-none'
+						}
 						return `<div style="width: 100px !important;" class="d-flex flex-column">
-												<a href="<?= $_ENV['ASSETS_URL'] ?>${data}?subfolder=resi_konsumsi&token=<?= $_ENV['ASSETS_TOKEN'] ?>" target="_blank" class="link-primary d-flex align-items-center">
-													<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-file-earmark-image" viewBox="0 0 16 16">
-														<path d="M6.502 7a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/>
-														<path d="M14 14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5L14 4.5V14zM4 1a1 1 0 0 0-1 1v10l2.224-2.224a.5.5 0 0 1 .61-.075L8 11l2.157-3.02a.5.5 0 0 1 .76-.063L13 10V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4z"/>
-													</svg>
-													<span class="ms-1">
-														See receipt
-													</span>
-												</a>
-												<p style="cursor: pointer;" class="link-primary d-flex align-items-center change-consumption-receipt mt-1">
-													<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-														<path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-														<path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-													</svg>
-													<span class="ms-1">
-														Change
-													</span>
-												</p>
-											</div>`
+                                        <div class="meal-receipt-container ${receiptDetail}">
+                                            <a href="<?= $_ENV['ASSETS_URL'] ?>${data}?subfolder=resi_konsumsi&token=<?= $_ENV['ASSETS_TOKEN'] ?>" target="_blank" class="link-primary d-flex align-items-center meal-receipt-image">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-file-earmark-image" viewBox="0 0 16 16">
+                                                    <path d="M6.502 7a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/>
+                                                    <path d="M14 14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5L14 4.5V14zM4 1a1 1 0 0 0-1 1v10l2.224-2.224a.5.5 0 0 1 .61-.075L8 11l2.157-3.02a.5.5 0 0 1 .76-.063L13 10V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4z"/>
+                                                </svg>
+                                                <span class="ms-1">
+                                                    See receipt
+                                                </span>
+                                            </a>
+                                            <p style="cursor: pointer;" class="link-primary d-flex align-items-center change-meal-receipt mt-1">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                                    <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                                                    <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                                                </svg>
+                                                <span class="ms-1">
+                                                    Change
+                                                </span>
+                                            </p>
+                                        </div>
+                                        <div class="meal-file-upload ${uploadBtn}">
+                                            <input type="file" data-id="${row[17]}" class="form-control meal-receipt-file small-file-upload">
+                                            <small class="my-2 upload-notif"></small>
+                                        </div>
+								</div>`
 					}
 				},
 				{
@@ -173,8 +195,15 @@
 					orderable: false,
 					searchable: false,
 					render: function (data, _, row) {
+						const receipt = row[14]
+						let receiptDetail = ''
+						let uploadBtn = 'd-none'
+						if (!receipt || receipt == '' || receipt == null) {
+							uploadBtn = ''
+							receiptDetail = 'd-none'
+						}
 						return `<div style="width: 100px !important;" class="d-flex flex-column">
-                                        <div class="transfer-receipt-container">
+                                        <div class="transfer-receipt-container ${receiptDetail}">
                                             <a href="<?= $_ENV['ASSETS_URL'] ?>${row[14]}?subfolder=transfer_receipt&token=<?= $_ENV['ASSETS_TOKEN'] ?>" target="_blank" class="link-primary d-flex align-items-center transfer-receipt-image">
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-file-earmark-image" viewBox="0 0 16 16">
                                                     <path d="M6.502 7a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/>
@@ -194,8 +223,8 @@
                                                 </span>
                                             </p>
                                         </div>
-                                        <div class="transfer-file-upload d-none">
-                                            <input type="file" data-id="${row[17]}" class="form-control transfer-receipt-file">
+                                        <div class="transfer-file-upload ${uploadBtn}">
+                                            <input type="file" data-id="${row[17]}" class="form-control transfer-receipt-file small-file-upload">
                                             <small class="my-2 upload-notif"></small>
                                         </div>
 								</div>`
@@ -221,9 +250,9 @@
 					type: 'POST',
 					url: base_url + 'absences/update_participant/' + id,
 					data: {
-                        value,
-                        field
-                    },
+						value,
+						field
+					},
 					error: function (xhr) {
 						const response = xhr.responseJSON;
 						showToast('Error', response.message, 'error')
@@ -241,10 +270,17 @@
 				const fileUpload = $this.parent().parent().find('.transfer-file-upload')
 				fileUpload.removeClass('d-none')
 			})
+			$(document).on('click', '.change-meal-receipt', function (e) {
+				const $this = $(this)
+				const receiptDetail = $this.parent()
+				receiptDetail.addClass('d-none')
+				const fileUpload = $this.parent().parent().find('.meal-file-upload')
+				fileUpload.removeClass('d-none')
+			})
 			$(document).on("change", '.transfer-receipt-file', function () {
 				var $this = $(this);
 				var form = new FormData();
-                const id = $this.attr('data-id')
+				const id = $this.attr('data-id')
 				form.append("file", $this[0].files[0]);
 				$.ajax({
 					url: asset_url + 'transfer_receipt',
@@ -280,7 +316,51 @@
 							receiptContainer.removeClass('d-none')
 							receiptContainer.find('.transfer-receipt-image').attr('href',
 								`<?= $_ENV['ASSETS_URL'] ?>${filename}?subfolder=transfer_receipt&token=<?= $_ENV['ASSETS_TOKEN'] ?>`
-								)
+							)
+						}
+					},
+				});
+			})
+			$(document).on("change", '.meal-receipt-file', function () {
+				var $this = $(this);
+				var form = new FormData();
+				const id = $this.attr('data-id')
+				form.append("file", $this[0].files[0]);
+				$.ajax({
+					url: asset_url + 'resi_konsumsi',
+					type: "POST",
+					data: form,
+					cache: false,
+					contentType: false,
+					processData: false,
+					dataType: 'json',
+					beforeSend: function (request) {
+						request.setRequestHeader("token",
+							"<?= $_ENV['ASSETS_TOKEN'] ?>");
+						$this.parent().find('.upload-notif').text('Uploading...!')
+					},
+					error: function (xhr) {
+						const response = xhr.responseJSON;
+						console.log(response)
+						$this.parent().find('.upload-notif').text(
+								'Failed to upload receipt!')
+							.addClass('text-danger')
+						$this.val(null);
+					},
+					success: function (response) {
+						if (response.status === true) {
+							const filename = response.data.filename
+							updateParticipantData(id, filename, 'resi_konsumsi')
+							$this.parent().find('.upload-notif').text(
+									'Receipt has been uploaded!')
+								.addClass('text-success')
+							$this.parent().addClass('d-none')
+							const receiptContainer = $this.parent().parent().find(
+								'.meal-receipt-container')
+							receiptContainer.removeClass('d-none')
+							receiptContainer.find('.meal-receipt-image').attr('href',
+								`<?= $_ENV['ASSETS_URL'] ?>${filename}?subfolder=resi_konsumsi&token=<?= $_ENV['ASSETS_TOKEN'] ?>`
+							)
 						}
 					},
 				});
